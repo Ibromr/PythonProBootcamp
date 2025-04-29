02ffee0bb68ab63eafd30b322276ac928ca20938
ibromr <omerogluibrahim@gmail.com>
2025-04-29 21:53:56 +1000

    Day 3 progressing

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,17 @@
+
+
+# PAUSE 1 - What is 10 % 3?
+# 1
+
+print(10%3)
+
+#PAUSE 2 - Check Odd or Even
+
+number_to_check = int(input("What is the number you want to check? "))
+
+
+if number_to_check%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,13 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+if size == "S":
+    bill = 15
+    if pepperoni == "Y":
+        bill += 2
+elif size == "M":
+    bill = 20
+elif size == "L":
+    bill = 25
+if pepperoni == "Y":
+    bill += 2
