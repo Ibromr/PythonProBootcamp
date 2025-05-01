f5b5f0ba5a936c143a7a3856b86dc7d3d6a11c71
ibromr <omerogluibrahim@gmail.com>
2025-05-01 21:59:58 +1000

    Day 3 completion

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -1,4 +1,4 @@
-print(r'''
+print('''
 *******************************************************************************
           |                   |                  |                     |
  _________|________________.=""_;=.______________|_____________________|_______
@@ -20,6 +20,9 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
+
+
+
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 move = input('You\'re at a crossroad, where do you want to go? \n'
@@ -32,5 +35,11 @@ elif move == "left":
     if move == "swim":
         print("You get attacked by an angry trout. Game Over.")
     elif move == "wait":
-        move = print("You arrive at the island unharmed. There is a house with 3 doors.\n"
-              "One red, one yellow and one blue. Which colour do yo choose")
+        print("You arrive at the island unharmed. There is a house with 3 doors.\n")
+        move = input("One red, one yellow and one blue. Which colour do yo choose\n")
+        if move == "red":
+            print("It\'s a room full of fire. Game Over.")
+        elif move == "yellow":
+            print("You found the treasure! you Win!")
+        else:
+            print("You enter a room of beasts. Game Over.")
